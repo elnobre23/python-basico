8260902c92b85928e50a27dd165c25e8ccbfcac9
Breno Cuzzuol <brenosoarescuzzuol@hotmail.com>
2023-04-20 10:56:14 -0300

    Add files via upload

---
+++ desconto.py
@@ -0,0 +1,16 @@
+preco_unitario = float(input('Insira o valor do preço unitário: '))
+desconto1 = 0.1
+desconto2 = 0.2
+unidade = float(input('Insira a quantidade de unidades: '))
+valor_desconto1 = (unidade * preco_unitario) * desconto1
+valor_desconto2 = (unidade * preco_unitario) * desconto2
+compra_1 = (preco_unitario * unidade) - valor_desconto1
+compra_2 = (preco_unitario * unidade) - valor_desconto2
+compra_3 = preco_unitario * unidade
+
+if unidade > 20:
+    print('O preço da sua compra foi de: R$ ', compra_2)
+elif unidade > 10 < 20:
+    print('O preço da sua compra foi de: R$ ', compra_1)
+else:
+    print('Não há descontos. O preço da sua compra foi de: R$ ', compra_3)
